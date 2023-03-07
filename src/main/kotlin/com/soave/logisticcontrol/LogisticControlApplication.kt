646194993bf8d56804521e0dc834195926155acb
ArthurSoave <atrpaula@gmail.com>
2023-03-07 08:37:09 -0300

    First project commit

---
+++ src/main/kotlin/com/soave/logisticcontrol/LogisticControlApplication.kt
@@ -0,0 +1,11 @@
+package com.soave.logisticcontrol
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LogisticControlApplication
+
+fun main(args: Array<String>) {
+	runApplication<LogisticControlApplication>(*args)
+}
---
+++ src/test/kotlin/com/soave/logisticcontrol/LogisticControlApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.soave.logisticcontrol
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LogisticControlApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
